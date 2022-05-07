1c6ab7742b740d31a210d0a17ead70a5421fc60b
Raynder Lobe <Raynder4@gmail.com>
2022-05-07 20:38:24 -0300

    iniciando template

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,15 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.copy('resources/css/adminlte.min.css', 'public/css/adminlte.min.css')
+    .copy('resources/css/adminlte.min.css.map', 'public/css/adminlte.min.css.map');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.copy('resources/js/adminlte.min.js', 'public/js/adminlte.min.js')
+    .copy('resources/js/adminlte.min.js.map', 'public/js/adminlte.min.js.map');
+
+mix.js('resources/js/app.js', 'public/js/app.js')
+    .sass('resources/sass/app.scss', 'public/css/app.css')
     .sourceMaps();
+
+mix.css('resources/css/app.css', 'public/css/app.css');
+
+mix.version();
